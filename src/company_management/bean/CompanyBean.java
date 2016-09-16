cdfa65f8bd35b4046d3702df6f525092f6013716
Gemcirelli <gemcirelli@gmail.com>
2016-09-16 14:00:29 -0400

    creacion de los bean de empleado y compañia

---
+++ src/company_management/bean/CompanyBean.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package company_management.bean;
+
+import java.util.ArrayList;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class CompanyBean implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8405457781559423830L;
+
+	private String idcompany;
+	private String company_name;
+	private String phone;
+	private String email;
+	
+	
+	public String getIdcompany() {
+		return idcompany;
+	}
+	public void setIdcompany(String idcompany) {
+		this.idcompany = idcompany;
+	}
+	public String getCompany_name() {
+		return company_name;
+	}
+	public void setCompany_name(String company_name) {
+		this.company_name = company_name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	} 
+	
+	
+	public ArrayList<EmployeeBean> getCompanyEmployees() {
+		return companyEmployees;
+	}
+	public void setCompanyEmployees(ArrayList<EmployeeBean> companyEmployees) {
+		this.companyEmployees = companyEmployees;
+	}
+
+
+	private ArrayList<EmployeeBean> companyEmployees;
+}
